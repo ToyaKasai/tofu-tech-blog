79ad30eae1a9c388fd9010cfd09855004a36e772
ToyaKasai <kasaitoya@team-lab.com>
2022-03-08 16:54:54 +0900

    update: 一旦保存できるようにする

--- app/Http/Controllers/TechBlogController.php
+++ app/Http/Controllers/TechBlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class TechBlogController extends Controller
@@ -14,5 +15,25 @@ class TechBlogController extends Controller
     public function create()
     {
         return view('blog.create');
+    }
+
+    public function store(Request $request)
+    {
+        $article = new Article();
+
+        // booleanをtinyInt型にする
+        $is_publish = $request->input('is_publish') === 'true' ? 1 : 0;
+        $is_save = $request->input('is_save') === 'true' ? 1 : 0;
+
+        $article->title = $request->input('title');
+        $article->description = $request->input('description');
+        $article->thumbnail_path = $request->input('thumbnail_path');
+        $article->source = $request->input('source');
+        $article->is_publish = $is_publish;
+        $article->is_save = $is_save;
+
+        $article->save();
+
+        return redirect('/');
     }
 }
