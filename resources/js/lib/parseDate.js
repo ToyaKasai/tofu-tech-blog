1dd092463f3d1a5f959a94d9c93a90558d3009be
ToyaKasai <kasaitoya@team-lab.com>
2022-03-12 18:57:33 +0900

    fix: format

--- resources/js/lib/parseDate.js
+++ resources/js/lib/parseDate.js
@@ -1,19 +1,19 @@
-import { format, parseISO } from "date-fns";
-import { ja } from "date-fns/locale";
+import {format, parseISO} from 'date-fns';
+import {ja} from 'date-fns/locale';
 
 const parseDate = (date, formatPattern) => {
-    try {
-        // ISOの形式に変換
-        const utcDate = parseISO(date);
+  try {
+    // ISOの形式に変換
+    const utcDate = parseISO(date);
 
-        return format(
-            new Date(utcDate),
-            formatPattern ? formatPattern : "yyyy年M月d日",
-            { locale: ja }
-        );
-    } catch {
-        return date;
-    }
+    return format(
+      new Date(utcDate),
+      formatPattern ? formatPattern : 'yyyy年M月d日',
+      {locale: ja},
+    );
+  } catch {
+    return date;
+  }
 };
 
 export default parseDate;
