ebfedf755fd36ee40b126bc363885a6d80ac8913
ToyaKasai <kasaitoya@team-lab.com>
2022-03-13 12:05:47 +0900

    update: 記事閲覧ページレスポンシブ対応

--- resources/js/components/pages/ViewPage.vue
+++ resources/js/components/pages/ViewPage.vue
@@ -52,196 +52,227 @@
 </template>
 
 <script>
-  import { computed, ref } from 'vue';
-  import {
-    STORAGE_IMAGE_PATH,
-    DEFAULT_THUMBNAIL_PATH,
-  } from '../../lib/storageImage';
-  import Icon from '../Icon.vue';
-  import MarkdownPreview from '../common/MarkdownPreview.vue';
-  import parseArticlePath from '../../lib/parseArticlePath';
-  import parseDate from '../../lib/parseDate';
-  import CsrfToken from '../common/form/CsrfToken.vue';
-  import axios from 'axios';
+import { computed, ref } from 'vue';
+import {
+  STORAGE_IMAGE_PATH,
+  DEFAULT_THUMBNAIL_PATH,
+} from '../../lib/storageImage';
+import Icon from '../Icon.vue';
+import MarkdownPreview from '../common/MarkdownPreview.vue';
+import parseArticlePath from '../../lib/parseArticlePath';
+import parseDate from '../../lib/parseDate';
+import CsrfToken from '../common/form/CsrfToken.vue';
+import axios from 'axios';
 
-  /** 記事閲覧ページ */
-  export default {
-    name: 'ViewPage',
-    components: {
-      Icon,
-      MarkdownPreview,
-      CsrfToken,
+/** 記事閲覧ページ */
+export default {
+  name: 'ViewPage',
+  components: {
+    Icon,
+    MarkdownPreview,
+    CsrfToken,
+  },
+  props: {
+    article: {
+      type: Object,
+      default: () => ({}),
     },
-    props: {
-      article: {
-        type: Object,
-        default: () => ({}),
-      },
-      csrf: {
-        type: String,
-        required: true,
-      },
-      deletePath: {
-        type: String,
-        required: true,
-      },
-      updateFavoriteUrl: {
-        type: String,
-        required: true,
-      },
+    csrf: {
+      type: String,
+      required: true,
     },
-    setup(props) {
-      const isSave = ref(props.article.is_save);
-      const isSaving = ref(false);
+    deletePath: {
+      type: String,
+      required: true,
+    },
+    updateFavoriteUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const isSave = ref(props.article.is_save);
+    const isSaving = ref(false);
 
-      const thumbnailSrc = computed(() =>
-        props.article.thumbnail_path
-          ? `${STORAGE_IMAGE_PATH}${props.article.thumbnail_path}`
-          : DEFAULT_THUMBNAIL_PATH,
-      );
+    const thumbnailSrc = computed(() =>
+      props.article.thumbnail_path
+        ? `${STORAGE_IMAGE_PATH}${props.article.thumbnail_path}`
+        : DEFAULT_THUMBNAIL_PATH,
+    );
 
-      const createDate = computed(() =>
-        parseDate(props.article.created_at, 'yyyy年M月d日 HH:mm'),
-      );
+    const createDate = computed(() =>
+      parseDate(props.article.created_at, 'yyyy年M月d日 HH:mm'),
+    );
 
-      const updateDate = computed(() =>
-        parseDate(props.article.updated_at, 'yyyy年M月d日 HH:mm'),
-      );
+    const updateDate = computed(() =>
+      parseDate(props.article.updated_at, 'yyyy年M月d日 HH:mm'),
+    );
 
-      const editPath = computed(() => parseArticlePath(props.article.id).edit);
+    const editPath = computed(() => parseArticlePath(props.article.id).edit);
 
-      const handleSubmit = (e) => {
-        if (!window.confirm('本当に削除しますか？')) {
-          e.preventDefault();
+    const handleSubmit = (e) => {
+      if (!window.confirm('本当に削除しますか？')) {
+        e.preventDefault();
+      }
+    };
+
+    /** お気に入り更新 */
+    const updateFavorite = async () => {
+      isSaving.value = true;
+      try {
+        const result = await axios.post(props.updateFavoriteUrl);
+        if (result.data === 1) {
+          isSave.value = !isSave.value;
         }
-      };
+      } catch (e) {
+        console.error(
+          `お気に入り更新処理に失敗しました。エラーメッセージ：${e}`,
+        );
+      }
+      isSaving.value = false;
+    };
 
-      /** お気に入り更新 */
-      const updateFavorite = async () => {
-        isSaving.value = true;
-        try {
-          const result = await axios.post(props.updateFavoriteUrl);
-          if (result.data === 1) {
-            isSave.value = !isSave.value;
-          }
-        } catch (e) {
-          console.error(
-            `お気に入り更新処理に失敗しました。エラーメッセージ：${e}`,
-          );
-        }
-        isSaving.value = false;
-      };
-
-      return {
-        isSave,
-        thumbnailSrc,
-        createDate,
-        updateDate,
-        editPath,
-        handleSubmit,
-        updateFavorite,
-      };
-    },
-  };
+    return {
+      isSave,
+      thumbnailSrc,
+      createDate,
+      updateDate,
+      editPath,
+      handleSubmit,
+      updateFavorite,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  @import '../../../sass/_mixins.scss';
+@import '../../../sass/_mixins.scss';
 
-  .view-page-wrapper {
+.view-page-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  @include mq('tablet') {
+    padding: 0 var(--margin-s);
+  }
+
+  @include mq('sp') {
+    padding: 0 var(--margin-xs);
+  }
+
+  > .link {
+    display: flex;
+    align-items: center;
+    column-gap: 8px;
+    width: var(--width-pc);
+    margin-top: var(--margin-m);
+    font-size: 1.4rem;
+    color: var(--base-text-color);
+
+    @include mq('tablet') {
+      width: 100%;
+      margin-left: var(--margin-s);
+    }
+
+    @include mq('sp') {
+      width: 100%;
+      margin-left: var(--margin-xs);
+    }
+  }
+
+  > .thumbnail {
+    height: 180px;
+    padding: var(--margin-m);
+    text-align: center;
+  }
+
+  > .thumbnail > .image {
+    height: 100%;
+  }
+
+  > .content {
+    width: var(--width-pc);
+
+    @include mq('tablet') {
+      width: 100%;
+    }
+  }
+
+  > .content > .date {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    column-gap: var(--margin-xs);
+  }
+
+  > .content > .date > .create,
+  > .content > .date > .update {
+    display: flex;
+    align-items: center;
+    column-gap: 8px;
+    font-size: 1.4rem;
+    color: var(--gray-600);
+    margin-top: var(--margin-xxs);
+  }
+
+  > .content > .title {
+    font-size: 2.8rem;
+    font-weight: bold;
+    text-align: center;
+  }
+}
+
+.article-content {
+  display: grid;
+  grid-template-columns: auto 1fr;
+  column-gap: var(--margin-m);
+  padding: var(--margin-xl) 0;
+
+  @include mq('sp') {
+    grid-template-columns: 1fr;
+    row-gap: var(--margin-xs);
+  }
+
+  > .side > .content {
+    position: sticky;
+    top: var(--margin-l);
     display: flex;
     flex-direction: column;
-    align-items: center;
-    justify-content: center;
+    row-gap: var(--margin-xs);
 
-    > .link {
-      display: flex;
-      align-items: center;
-      column-gap: 8px;
-      width: var(--width-pc);
-      margin-top: var(--margin-m);
-      font-size: 1.4rem;
-      color: var(--base-text-color);
-    }
-
-    > .thumbnail {
-      height: 180px;
-      padding: var(--margin-m);
-      text-align: center;
-    }
-
-    > .thumbnail > .image {
-      height: 100%;
-      // object-fit: cover;
-      // object-position: 50% 50%;
-    }
-
-    > .content {
-      width: var(--width-pc);
-    }
-
-    > .content > .date {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      column-gap: var(--margin-xs);
-    }
-
-    > .content > .date > .create,
-    > .content > .date > .update {
-      display: flex;
-      align-items: center;
-      column-gap: 8px;
-      font-size: 1.4rem;
-      color: var(--gray-600);
-      margin-top: var(--margin-xxs);
-    }
-
-    > .content > .title {
-      font-size: 2.8rem;
-      font-weight: bold;
-      text-align: center;
+    @include mq('sp') {
+      position: inherit;
+      flex-direction: row;
+      column-gap: var(--margin-xxs);
     }
   }
+}
 
-  .article-content {
-    display: grid;
-    grid-template-columns: auto 1fr;
-    column-gap: var(--margin-m);
-    padding: var(--margin-xl) 0;
+.action-button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  height: 40px;
+  color: var(--base-text-color);
+  background-color: var(--base-bg-color);
+  outline: 1px solid var(--base-border-color);
+  border-radius: var(--border-radius-circle);
 
-    > .side > .content {
-      position: sticky;
-      top: var(--margin-l);
-      display: flex;
-      flex-direction: column;
-      row-gap: var(--margin-xs);
-    }
+  @include np-shadow(3px, 6px);
+
+  &:hover {
+    @include np-inner-shadow(3px, 6px);
   }
 
-  .action-button {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 40px;
-    height: 40px;
-    color: var(--base-text-color);
-    background-color: var(--base-bg-color);
-    outline: 1px solid var(--base-border-color);
-    border-radius: var(--border-radius-circle);
-
-    @include np-shadow(3px, 6px);
-
-    &:hover {
-      @include np-inner-shadow(3px, 6px);
-    }
-
-    > .heart {
-      color: var(--red);
-    }
-
-    > .delete {
-      color: var(--alert);
-    }
+  > .heart {
+    color: var(--red);
   }
+
+  > .delete {
+    color: var(--alert);
+  }
+}
 </style>
